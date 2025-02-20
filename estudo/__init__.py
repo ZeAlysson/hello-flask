2e279c885c53af4ad813bdc771e3f0b0580e4392
ZeAlysson <alyssonlogar123@gmail.com>
2025-02-20 19:53:31 -0300

    Variaveis de ambient e python-dotenv: sistema mais seguro

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # import the SQLAlchemy class from the flask_sqlalchemy module
 from flask_migrate import Migrate  # import the Migrate class from the flask_migrate module
+from dotenv import load_dotenv  # import the load_dotenv function from the dotenv module
+import os  # import the os module
+load_dotenv('.env')  # load the .env file
 
 app = Flask(__name__)  # create an instance of the Flask class
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # set the database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')  # set the database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # set the track modifications to False
-app.config['SECRET_KEY'] = 'wd4jh534nk'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # set the secret key
+
 db = SQLAlchemy(app)  # create an instance of the SQLAlchemy class
 migrate = Migrate(app, db)  # create an instance of the Migrate class
 
